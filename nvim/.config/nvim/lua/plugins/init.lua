d8770a10218fe2e543b86476d0614495835b9bc7
Lance-N3rdlov3 <n3rdlov3@n3rdlabs.dev>
2025-01-02 00:54:36 -0500

    update
    drwx------     - nrd  19 Dec  2024   gnome-session    drwxrwxr-x     - nrd  19 Dec  2024   zellij
    drwx------     - nrd  19 Dec  2024   gnome-session    drwxrwxr-x     - nrd  19 Dec  2024   zellij

---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,112 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    ---   event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    -- refer to `:h file-pattern` for more examples
+    ---    "BufReadPre /Users/nrd/vaults/code/*.md",
+    --
+    ---   "BufNewFile /Users/nrd/vaults/code/*.md",
+    --- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "hrsh7th/nvim-cmp",
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/vaults/code",
+        },
+        ---{
+        ---name = "work",
+        --path = "~/vaults/work",
+        ---},
+      },
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+      },
+      mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- Toggle check-boxes.
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        -- Smart action depending on context, either follow link or toggle checkbox.
+        ["<cr>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        },
+      },
+      new_notes_location = "current_dir",
+      wiki_link_func = function(opts)
+        return require("obsidian.util").wiki_link_id_prefix(opts)
+      end,
+
+      -- Optional, customize how markdown links are formatted.
+      markdown_link_func = function(opts)
+        return require("obsidian.util").markdown_link(opts)
+      end,
+
+      -- Either 'wiki' or 'markdown'.
+      preferred_link_style = "wiki",
+
+      -- Optional, boolean or a function that takes a filename and returns a boolean.
+      -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+      disable_frontmatter = false,
+      -- see below for full list of options 👇
+    },
+    require("lazy").setup {
+      {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+          local configs = require "nvim-treesitter.configs"
+
+          configs.setup {
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+          }
+        end,
+      },
+    },
+  },
+}
